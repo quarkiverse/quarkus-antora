b0c3817b10e08fc8796f0423250d04c52cf3edfa
Peter Palaga <ppalaga@redhat.com>
2025-03-26 14:35:25 +0100

    Read '3.20' as a string without quotes

--- antorassured/src/test/java/io/quarkiverse/antorassured/ResourceResolverTest.java
+++ antorassured/src/test/java/io/quarkiverse/antorassured/ResourceResolverTest.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.Yaml;
 
 import io.quarkiverse.antorassured.ResourceResolver.ResourceResolverImpl;
 
@@ -30,6 +31,48 @@ public class ResourceResolverTest {
                 .isEqualTo("http://localhost:8081/page.html");
         Assertions.assertThat(r.resolveUri(Path.of("/foo/bar/site/folder/index.html"), "../page.html").resolvedUri())
                 .isEqualTo("http://localhost:8081/page.html");
+    }
+
+    @Test
+    void findKeyVersion() {
+        final Map<String, Object> yaml = new Yaml()
+                .load("""
+                        name: quarkus-cxf
+                        title: CXF
+                        version: '3.20'
+                        nav:
+                          - modules/ROOT/nav.adoc
+
+                        asciidoc:
+                          attributes:
+
+                            # Versions
+                            quarkus-version: 3.20.0 # replace ${quarkus.version}
+                            quarkus-cxf-version: 3.20.0 # replace ${release.current-version}
+
+                            # Toggle whether some page elements are rendered
+                            doc-show-badges: true # Whether JVM / Native badges are rendered
+                            doc-show-advanced-features: true # Whether documentation relating to advanced features is rendered
+                            doc-show-user-guide-link: true # Whether the user guide link is rendered in component extensions
+                            doc-show-extra-content: false # Whether additional content hosted in external AsciiDoc files should be loaded via an include:: directive
+                            doc-is-main: true # Release notes and release planning are only shown and maintained in the main branch
+
+                            quarkus-cxf-project-name: Quarkus CXF
+
+                            # External URLs
+                            link-quarkus-cxf-source-tree-base: https://github.com/quarkiverse/quarkus-cxf/tree/main
+                            link-quarkus-code-generator: code.quarkus.io
+                            link-camel-quarkus-docs-index: https://camel.apache.org/camel-quarkus/latest/index.html
+                            link-camel-quarkus-docs-cxf-soap: https://camel.apache.org/camel-quarkus/latest/reference/extensions/cxf-soap.html
+                            link-quarkus-docs-base: https://quarkus.io/guides
+
+                            # Misc
+                            javaxOrJakartaPackagePrefix: jakarta # this can be switched to javax in older branches
+                                        """);
+
+        Assertions.assertThat(ResourceResolverImpl.findKey(yaml, "name")).isEqualTo("quarkus-cxf");
+        Assertions.assertThat(ResourceResolverImpl.findKey(yaml, "version")).isEqualTo("3.20");
+
     }
 
 }
