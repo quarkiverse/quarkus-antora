bdd81ec3126d56855799407be7938a5537b5976f
Peter Palaga <ppalaga@redhat.com>
2025-05-02 23:42:42 +0200

    Add Rate limit to workaround #109 429 Too Many Requests when GET-ting https://github.com/<org>/<repo>/(?:blob|tree)/.* links

--- antorassured/src/main/java/io/quarkiverse/antorassured/LinkStream.java
+++ antorassured/src/main/java/io/quarkiverse/antorassured/LinkStream.java
@@ -6,13 +6,14 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import org.jboss.logging.Logger;
+
+import io.quarkiverse.antorassured.LinkValidator.LinkValidatorImpl;
 
 /**
  * A stream of {@link Link}s.
@@ -24,12 +25,17 @@ public class LinkStream {
     private final Stream<Link> links;
     private final ResourceResolver resourceResolver;
     private final int retryAttempts;
+    private final List<RateLimitEntry> rateLimits;
+    private long overallTimeout;
 
-    LinkStream(Stream<Link> links, ResourceResolver resourceResolver, int retryAttempts) {
+    LinkStream(Stream<Link> links, ResourceResolver resourceResolver, int retryAttempts, List<RateLimitEntry> rateLimits,
+            long overallTimeout) {
         super();
         this.links = links;
         this.resourceResolver = resourceResolver;
         this.retryAttempts = retryAttempts;
+        this.rateLimits = rateLimits;
+        this.overallTimeout = overallTimeout;
     }
 
     /**
@@ -50,7 +56,9 @@ public class LinkStream {
         return new LinkStream(
                 links.peek(link -> AntorAssured.log.info(link)),
                 resourceResolver,
-                retryAttempts);
+                retryAttempts,
+                rateLimits,
+                overallTimeout);
     }
 
     /**
@@ -71,7 +79,8 @@ public class LinkStream {
      * @since 1.0.0
      */
     public LinkStream exclude(Predicate<Link> exclude) {
-        return new LinkStream(links.filter(link -> !exclude.test(link)), resourceResolver, retryAttempts);
+        return new LinkStream(links.filter(link -> !exclude.test(link)), resourceResolver, retryAttempts, rateLimits,
+                overallTimeout);
     }
 
     /**
@@ -84,7 +93,7 @@ public class LinkStream {
      */
     public LinkStream excludeResolved(Pattern exclude) {
         return new LinkStream(links.filter(link -> !exclude.matcher(link.resolvedUri()).matches()), resourceResolver,
-                retryAttempts);
+                retryAttempts, rateLimits, overallTimeout);
     }
 
     /**
@@ -110,7 +119,8 @@ public class LinkStream {
      * @since 1.0.0
      */
     public LinkStream excludeResolved(Collection<String> excludes) {
-        return new LinkStream(links.filter(link -> !excludes.contains(link.resolvedUri())), resourceResolver, retryAttempts);
+        return new LinkStream(links.filter(link -> !excludes.contains(link.resolvedUri())), resourceResolver, retryAttempts,
+                rateLimits, overallTimeout);
     }
 
     /**
@@ -122,7 +132,7 @@ public class LinkStream {
      */
     public LinkStream includeResolved(Pattern include) {
         return new LinkStream(links.filter(link -> include.matcher(link.resolvedUri()).matches()), resourceResolver,
-                retryAttempts);
+                retryAttempts, rateLimits, overallTimeout);
     }
 
     /**
@@ -134,16 +144,40 @@ public class LinkStream {
      * @since 1.3.0
      */
     public LinkStream retryAttempts(int retryAttempts) {
-        return new LinkStream(links, resourceResolver, retryAttempts);
+        return new LinkStream(links, resourceResolver, retryAttempts, rateLimits, overallTimeout);
     }
 
     /**
-     * @return same as {@code validate(LinkValidator.defaultValidator())}
+     * @param overallTimeout a timeout in milliseconds within which all links must get validated including any retries; if not
+     *        set, the default value is 30000 milliseconds
+     * @return a new {@link LinkStream} with {@link #overallTimeout} reset to the given value
+     *
+     * @since 1.3.0
+     */
+    public LinkStream overallTimeout(long overallTimeout) {
+        return new LinkStream(links, resourceResolver, retryAttempts, rateLimits, overallTimeout);
+    }
+
+    /**
+     * @param regExp a regular expression, the given {@code rateLimit} will be applied to matching URI's
+     * @param rateLimit a {@link RateLimit}, such as {@link RateLimit#requestsPerTimeInterval(int, long)}
+     * @return a new {@link LinkStream} with {@link RateLimit} set to matching URIs.
+     *
+     * @since 1.4.0
+     */
+    public LinkStream rateLimit(String regExp, RateLimit rateLimit) {
+        final List<RateLimitEntry> newRateLimits = new ArrayList<>(rateLimits);
+        newRateLimits.add(new RateLimitEntry(Pattern.compile(regExp), rateLimit));
+        return new LinkStream(links, resourceResolver, retryAttempts, newRateLimits, overallTimeout);
+    }
+
+    /**
+     * @return a {@link ValidationErrorStream} using {@link LinkValidatorImpl}.
      *
      * @since 1.0.0
      */
     public ValidationErrorStream validate() {
-        return validate(LinkValidator.defaultValidator());
+        return validate(new LinkValidatorImpl(rateLimits, retryAttempts));
     }
 
     /**
@@ -159,42 +193,50 @@ public class LinkStream {
                 .map(validator::validate)
                 .filter(ValidationResult::isInvalid)
                 .forEach(result -> {
-                    (result.shouldRetry() ? invalidWithRetry : invalidWithouRetry).add(result);
+                    if (result.shouldRetry()) {
+                        synchronized (invalidWithRetry) {
+                            invalidWithRetry.add(result);
+                        }
+                    } else {
+                        synchronized (invalidWithouRetry) {
+                            invalidWithouRetry.add(result);
+                        }
+                    }
                 });
 
-        if (!invalidWithRetry.isEmpty() && retryAttempts > 0) {
+        while (!invalidWithRetry.isEmpty()) {
             /* sort the retries by retry time */
             Collections.sort(invalidWithRetry, Comparator.comparing(ValidationResult::retryAtSystemTimeMs));
 
             /* Retry */
-            for (int attempt = 2; attempt <= retryAttempts + 1; attempt++) {
-                final ListIterator<ValidationResult> it = invalidWithRetry.listIterator();
-                while (it.hasNext()) {
-                    final ValidationResult oldResult = it.next();
-                    final long delay = oldResult.retryAtSystemTimeMs() - System.currentTimeMillis();
-                    if (delay > 0L) {
-                        try {
-                            log.infof("Sleeping %d ms to retry %s", delay, oldResult);
-                            Thread.sleep(delay);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    final ValidationResult newResult = validator.validate(oldResult.uri(), attempt);
-                    if (newResult.isValid()) {
-                        it.remove();
-                    } else if (newResult.shouldRetry()) {
-                        it.set(newResult);
-                    } else {
-                        it.remove();
-                        invalidWithRetry.add(newResult);
-                    }
+            final ValidationResult oldResult = invalidWithRetry.get(0);
+            final long delay = oldResult.retryAtSystemTimeMs() - System.currentTimeMillis();
+            if (delay > 0L) {
+                try {
+                    log.infof("Sleeping %d ms to retry %s; there are still %d URIs to retry ", delay, oldResult,
+                            invalidWithRetry.size());
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException(e);
                 }
+            }
+            final ValidationResult newResult = validator.validate(oldResult.uri());
+            if (newResult.isValid()) {
+                invalidWithRetry.remove(0);
+            } else if (newResult.shouldRetry()) {
+                invalidWithRetry.remove(0);
+                invalidWithRetry.add(newResult);
+            } else {
+                invalidWithRetry.remove(0);
+                invalidWithouRetry.add(newResult);
             }
         }
         return new ValidationErrorStream(
                 Stream.concat(invalidWithouRetry.stream(), invalidWithRetry.stream()),
                 resourceResolver);
+    }
+
+    static record RateLimitEntry(Pattern pattern, RateLimit rateLimit) {
     }
 }
