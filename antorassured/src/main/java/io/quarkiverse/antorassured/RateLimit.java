e55ed97ca2985ab4d2355f8a956d28755642f2eb
Peter Palaga <ppalaga@redhat.com>
2025-05-02 23:42:42 +0200

    Add Rate limit to workaround #109 429 Too Many Requests when GET-ting https://github.com/<org>/<repo>/(?:blob|tree)/.* links

---
+++ antorassured/src/main/java/io/quarkiverse/antorassured/RateLimit.java
@@ -0,0 +1,73 @@
+package io.quarkiverse.antorassured;
+
+import java.time.Clock;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * A rate limit for deciding when a request to some specific URI can be scheduled.
+ *
+ * @since 1.4.0
+ */
+public interface RateLimit {
+
+    /**
+     * @param requestCountLimit how many requests can be sent during the time window defined by {@code resetIntervalMillis}
+     * @param resetIntervalMillis number of milliseconds
+     * @return a new {@link RateLimit} scheduling requests by number if requests per time interval per given URI
+     */
+    static RateLimit requestsPerTimeInterval(int requestCountLimit, long resetIntervalMillis) {
+        return new RequestsPerTimeRateLimit(requestCountLimit, resetIntervalMillis);
+    }
+
+    /**
+     * @param uri the URL to decide about
+     * @return delay in milliseconds after which the URL can be accessed
+     */
+    long scheduleInMilliseconds(String uri);
+
+    static class RequestsPerTimeRateLimit implements RateLimit {
+
+        RequestsPerTimeRateLimit(int requestCountLimit, long resetIntervalMillis, Clock clock) {
+            super();
+            this.requestCountLimit = requestCountLimit;
+            this.resetIntervalMillis = resetIntervalMillis;
+            this.clock = clock;
+        }
+
+        RequestsPerTimeRateLimit(int requestCountLimit, long resetIntervalMillis) {
+            this(requestCountLimit, resetIntervalMillis, Clock.systemUTC());
+        }
+
+        private final int requestCountLimit;
+        private final long resetIntervalMillis;
+        private final Clock clock;
+        private final Map<String, Counter> counters = new ConcurrentHashMap<>();
+
+        @Override
+        public long scheduleInMilliseconds(String uri) {
+            final Counter cnt = counters.computeIfAbsent(uri, k -> new Counter());
+            synchronized (cnt) {
+                if (++cnt.count <= requestCountLimit) {
+                    return 0;
+                }
+                final long elapsedTime = clock.millis() - cnt.timestamp;
+                if (elapsedTime >= resetIntervalMillis) {
+                    cnt.reset();
+                    return 0;
+                }
+                return resetIntervalMillis - elapsedTime;
+            }
+        }
+
+        class Counter {
+            private int count = 0;
+            private long timestamp = clock.millis();
+
+            void reset() {
+                count = 1;
+                timestamp = clock.millis();
+            }
+        }
+    }
+}
